d344e087110889335a040922a68b6b646f82b51b
Vijay <vijayausare0007@gmail.com>
2023-07-04 17:38:09 +0530

    Created hero interface and created hero.ts  and imported Hero interface

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core';  //  always import from core angular library
+import { Hero } from './hero';
 
-@Component({
+@Component({   // decorator function 
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent {
-
+  hero: Hero = {
+    id : 1, 
+    name : "Hero1"
+  } 
 }
